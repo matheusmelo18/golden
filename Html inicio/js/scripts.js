042ff879b08613f991fc94882471c688fa7dee5c
ViniciusPetersen <112730065+ViniciusPetersen@users.noreply.github.com>
2023-08-29 16:15:14 -0300

    nova api no script

--- Html inicio/js/scripts.js
+++ Html inicio/js/scripts.js
@@ -1,27 +1,45 @@
-const button = document.querySelector('.b-converter');
+
+    // Seu código JavaScript aqui
+
+const buttonC = document.querySelector('.b-converter');
+const buttonS = document.querySelector('.b-switch')
 const inputV = document.querySelector('.i-real');
-const inputC1 = document.querySelector('.moeda1');
-const inputC2 = document.querySelector('.moeda2');
 const select1 = document.querySelector('.moedas');
 const select2 = document.querySelector('.moedas2');
 
-const url = 'https://economia.awesomeapi.com.br/last/';
+const url = 'https://v6.exchangerate-api.com/v6/2b28f847f6610668cfba5704/latest/'
+const today = new Date();
 
-button.addEventListener('click', function() {
-    const coins = select1.value + '-' + select2.value;
+function formatDate(date, format) {
+	format.replace('mm', date.getMonth() + 1)
+    .replace('aa', date.getFullYear())
+	.replace('dd', date.getDate());
+}
+
+
+buttonC.addEventListener('click', function() {
+    const coins = select1.value;
 
     fetch(url + coins)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            const coin = select1.value + select2.value;
-            const conversao = data[coin];
+            console.log(data);
+            const coin =  select2.value;
+            const conversao = data.conversion_rates[coin];
 
-            document.getElementById('dol').innerHTML = parseFloat(inputV.value / conversao.bid).toLocaleString('en', {
+            document.getElementById('dol').innerHTML = parseFloat(inputV.value / conversao).toLocaleString('en', {
                 style: 'currency',
                 currency: select1.value
             });
         });
+});
+
+buttonS.addEventListener('click', function() {
+    const auxIndex = select1.selectedIndex;
+    select1.selectedIndex = select2.selectedIndex;
+    select2.selectedIndex = auxIndex;
+    
 });
 
